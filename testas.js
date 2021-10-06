e472f405449beefae7385b393b13fe189a63e32b
gretapos <86227872+gretapos@users.noreply.github.com>
2021-10-06 13:59:41 +0300

    kiek spejau

--- testas.js
+++ testas.js
@@ -1,17 +1,36 @@
 //1. Duotas masyvas, console.log atspausdinti masyvo ilgį
 const mas1 = [12, 5, 9, 7, 5, 4];
- 
+console.log(mas1.length);
+console.log('------------------------------------');
+
 //2. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę;
 const mas2 = [13, '785', 92, 'labas', 522, 6984];
- 
+for (var i = 0; i < mas2.length; i++) {
+    console.log('Indeksas: ' + i + ' Reiksme: ' + mas2[i]);
+}
+console.log('------------------------------------');
+
 //3. Duotas masyvas, masyvą iteruoti ciklu for ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė yra lyginė (porinė)
 const mas3 = [889, 52, 698, 752, 9333, 7];
- 
+for (var i = 0; i < mas3.length; i++){
+    if (mas3[i]%2==0) {
+        console.log('Indeksas: ' + i + ' Reiksme: ' + mas3[i]);
+    }
+}
+console.log('------------------------------------');
+
 //4. Duotas masyvas, masyvą iteruoti ciklu forEach ir console.log kiekvienoje
 // iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė prasideda raide A arba a
 const mas4 = ['Agurkas', 'Bananas', 'arbūzas', 'Bebras', 'Voverė', 'Ąžuolas', 'Mažas triušis'];
+mas4.forEach( (element, index) => {
+    if (element.startsWith('A') || element.startsWith('a')) {
+        console.log('Indeksas: ' + index + ' Reiksme: ' + element);  
+    }
+});
+console.log('------------------------------------');
+
  
 //5. Duotas stringas, suskaičiuoti kiek stringe yra raidžių z
 const str1 = 'Storas zebras, bėga nuo zuulu genties nedžiotojų.';
@@ -19,6 +38,9 @@ const str1 = 'Storas zebras, bėga nuo zuulu genties nedžiotojų.';
 //6. Duotas Setas, į kurį reikia pridėti reikšmes (stringus) 'Labas' ir 'Ate' ir console.log
 // atspausdinti Seto ilgį
 const set1 = new Set(['Labas', 'Vakaras', 'Rytas', 'Valio']);
+set1.add('Labas');
+set1.add('Ate');
+console.log(set1.values())
  
 //7. Suprototaipinti standartinį String tipo objektą, pridedant metodą lastCharIs(), 
 // kuris grąžintų paskutinį stringo simbolį. Pademonstruoti veikimą su stringu 'Ku Ku';
